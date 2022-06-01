279067f7a2602ca73436f1e076f53855ea8cea40
Connor Day <connor.t.day@gmail.com>
2022-06-01 13:41:23 -0400

    Clean up default react project

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { CollapsableContainer } from "./components/CollapsableContainer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props: { [key: string]: any }) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="app">
+                <div className="header">
+                    <p> hello world </p>
+                </div>
+                <div className="body">
+                    <CollapsableContainer />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
