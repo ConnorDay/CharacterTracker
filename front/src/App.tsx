5a1f5b5c7d118bdefb3b16157597ec9687c255ad
Connor Day <connor.t.day@gmail.com>
2022-06-08 20:04:47 -0400

    App now routes to different pages. CharacterSheet is what used to be the default page

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,84 +1,21 @@
-import React from "react";
 import "./App.css";
-import { Bio } from "./components/Bio";
-import { CollapsibleContainer } from "./components/CollapsibleContainer";
-import { Column } from "./components/Column";
-import { Saves } from "./components/Saves";
-import { Skills } from "./components/Skills";
-import { Stats } from "./components/Stats";
+import CharacterSheet from "./routes/CharacterSheet/CharacterSheet";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-    const testData = {
-        bio: { name: "test", maxHp: 40, currHp: 20 },
-        skills: [
-            {
-                name: "athletics",
-                value: 4,
-            },
-            {
-                name: "acrobatics",
-                value: 4,
-            },
-            {
-                name: "fegli",
-                value: 4,
-            },
-            {
-                name: "diesel",
-                value: 4,
-            },
-        ],
-        saves: [
-            {
-                name: "fortitude",
-                value: -1,
-            },
-            {
-                name: "horny",
-                value: -99,
-            },
-            {
-                name: "wis",
-                value: 1,
-            },
-        ],
-        stats: [
-            {
-                name: "str",
-                value: 10,
-            },
-            {
-                name: "dex",
-                value: 9,
-            },
-            {
-                name: "con",
-                value: 15,
-            },
-            {
-                name: "int",
-                value: 12,
-            },
-        ],
-    };
     return (
-        <div className="app">
-            <div className="header">
-                <p> hello world </p>
+        <BrowserRouter>
+            <div className="app">
+                <div className="header">
+                    <p> hello world </p>
+                </div>
+
+                <Routes>
+                    <Route path="/" element={<>test</>} />
+                    <Route path="/character/:id" element={<CharacterSheet />} />
+                </Routes>
             </div>
-            <div className="body">
-                <Column>
-                    <Stats stats={testData.stats} />
-                    <Saves saves={testData.saves} />
-                </Column>
-                <Column>
-                    <Bio {...testData.bio} />
-                </Column>
-                <Column>
-                    <Skills skills={testData.skills} />
-                </Column>
-            </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
---
+++ front/src/routes/CharacterSheet/CharacterSheet.tsx
@@ -0,0 +1,79 @@
+import { useParams } from "react-router-dom";
+import { Bio } from "../../components/Bio";
+import { Column } from "../../components/Column";
+import { Saves } from "../../components/Saves";
+import { Skills } from "../../components/Skills";
+import { Stats } from "../../components/Stats";
+
+function CharacterSheet() {
+    const testData = {
+        bio: { name: "test", maxHp: 40, currHp: 20 },
+        skills: [
+            {
+                name: "athletics",
+                value: 4,
+            },
+            {
+                name: "acrobatics",
+                value: 4,
+            },
+            {
+                name: "fegli",
+                value: 4,
+            },
+            {
+                name: "diesel",
+                value: 4,
+            },
+        ],
+        saves: [
+            {
+                name: "fortitude",
+                value: -1,
+            },
+            {
+                name: "horny",
+                value: -99,
+            },
+            {
+                name: "wis",
+                value: 1,
+            },
+        ],
+        stats: [
+            {
+                name: "str",
+                value: 10,
+            },
+            {
+                name: "dex",
+                value: 9,
+            },
+            {
+                name: "con",
+                value: 15,
+            },
+            {
+                name: "int",
+                value: 12,
+            },
+        ],
+    };
+    const params = useParams();
+    return (
+        <div className="body">
+            {params.id}
+            <Column>
+                <Stats stats={testData.stats} />
+                <Saves saves={testData.saves} />
+            </Column>
+            <Column>
+                <Bio {...testData.bio} />
+            </Column>
+            <Column>
+                <Skills skills={testData.skills} />
+            </Column>
+        </div>
+    );
+}
+export default CharacterSheet;
