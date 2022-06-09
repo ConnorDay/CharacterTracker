ebe1aafa5da537b054cd742c993e76363cf2127d
Connor Day <connor.t.day@gmail.com>
2022-06-09 17:19:29 -0400

    App now makes an axios get call to get character data

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import internal from "stream";
 import "./App.css";
 import { Bio } from "./components/Bio";
 import { CollapsibleContainer } from "./components/CollapsibleContainer";
@@ -7,61 +9,36 @@ import { Saves } from "./components/Saves";
 import { Skills } from "./components/Skills";
 import { Stats } from "./components/Stats";
 
-function App() {
-    console.log(process.env.REACT_APP_API_TARGET);
-    const testData = {
-        bio: { name: "test", maxHp: 40, currHp: 20 },
-        skills: [
-            {
-                name: "athletics",
-                value: 4,
-            },
-            {
-                name: "acrobatics",
-                value: 4,
-            },
-            {
-                name: "fegli",
-                value: 4,
-            },
-            {
-                name: "diesel",
-                value: 4,
-            },
-        ],
-        saves: [
-            {
-                name: "fortitude",
-                value: -1,
-            },
-            {
-                name: "horny",
-                value: -99,
-            },
-            {
-                name: "wis",
-                value: 1,
-            },
-        ],
-        stats: [
-            {
-                name: "str",
-                value: 10,
-            },
-            {
-                name: "dex",
-                value: 9,
-            },
-            {
-                name: "con",
-                value: 15,
-            },
-            {
-                name: "int",
-                value: 12,
-            },
-        ],
+type entry = {
+    name: string;
+    value: number;
+};
+
+type characterResponse = {
+    bio: {
+        name: string;
+        maxHp: number;
+        currHp: number;
     };
+    skills: entry[];
+    stats: entry[];
+    saves: entry[];
+};
+function App() {
+    const [character, setCharacter] = useState<characterResponse | undefined>(
+        undefined
+    );
+    useEffect(() => {
+        axios.get("/api/character/1").then((data) => {
+            console.log(data);
+            setCharacter(data.data);
+        });
+    }, []);
+
+    if (character === undefined) {
+        return <div className="body">waiting on data</div>;
+    }
+
     return (
         <div className="app">
             <div className="header">
@@ -69,14 +46,14 @@ function App() {
             </div>
             <div className="body">
                 <Column>
-                    <Stats stats={testData.stats} />
-                    <Saves saves={testData.saves} />
+                    <Stats stats={character.stats} />
+                    <Saves saves={character.saves} />
                 </Column>
                 <Column>
-                    <Bio {...testData.bio} />
+                    <Bio {...character.bio} />
                 </Column>
                 <Column>
-                    <Skills skills={testData.skills} />
+                    <Skills skills={character.skills} />
                 </Column>
             </div>
         </div>
