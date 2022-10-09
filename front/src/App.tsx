a23c9e9bfb782016bb528e19161a85296fc07603
Connor Day <connor.t.day@gmail.com>
2022-10-09 15:18:08 -0400

    Added a much nicer navbar

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,37 +3,43 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { CharacterList, CharacterSheet } from "./routes/Character";
 import { Campaign, CampaignList } from "./routes/Campaign";
 import { RulesetList, Ruleset } from "./routes/Ruleset";
+import { CustNavbar } from "./components/CustNavbar";
 
 function App() {
     return (
-        <BrowserRouter>
-            <div className="app">
-                <div className="header">
-                    <p> hello world </p>
+        <>
+            <CustNavbar />
+            <BrowserRouter>
+                <div className="app">
+                    <Routes>
+                        <Route path="/">
+                            <Route index element={<>test</>} />
+                            <Route path="character">
+                                <Route index element={<CharacterList />} />
+                                <Route
+                                    path=":character_id"
+                                    element={<CharacterSheet />}
+                                />
+                            </Route>
+                            <Route path="campaign">
+                                <Route index element={<CampaignList />} />
+                                <Route
+                                    path=":campaign_id"
+                                    element={<Campaign />}
+                                />
+                            </Route>
+                            <Route path="ruleset">
+                                <Route index element={<RulesetList />} />
+                                <Route
+                                    path=":ruleset_id"
+                                    element={<Ruleset />}
+                                />
+                            </Route>
+                        </Route>
+                    </Routes>
                 </div>
-
-                <Routes>
-                    <Route path="/">
-                        <Route index element={<>test</>} />
-                        <Route path="character">
-                            <Route index element={<CharacterList />} />
-                            <Route
-                                path=":character_id"
-                                element={<CharacterSheet />}
-                            />
-                        </Route>
-                        <Route path="campaign">
-                            <Route index element={<CampaignList />} />
-                            <Route path=":campaign_id" element={<Campaign />} />
-                        </Route>
-                        <Route path="ruleset">
-                            <Route index element={<RulesetList />} />
-                            <Route path=":ruleset_id" element={<Ruleset />} />
-                        </Route>
-                    </Route>
-                </Routes>
-            </div>
-        </BrowserRouter>
+            </BrowserRouter>
+        </>
     );
 }
 
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
